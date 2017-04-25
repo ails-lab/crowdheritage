4fdcff28330c13e527a5bd671738800b83234edf
Spyros Bekiaris <spirosbekos@gmail.com>
2017-04-25 18:04:42 +0300

    Start setting up item page

--- src/pages/item/campaignItem.js
+++ src/pages/item/campaignItem.js
@@ -14,9 +14,44 @@
  */
 
 
+import { inject } from 'aurelia-framework';
+import { Campaign } from '../../modules/Campaign.js';
+import { CampaignServices } from '../../modules/CampaignServices.js';
+import { Collection } from '../../modules/Collection.js';
+import { CollectionServices } from '../../modules/CollectionServices.js';
+
+@inject(CampaignServices, CollectionServices)
 export class CampaignItem {
+
+  constructor(campaignServices, collectionServices) {
+    this.campaignServices = campaignServices;
+    this.collectionServices = collectionServices;
+    this.campaign = 0;
+    this.collection = 0;
+    this.collectionCount = 0;
+    this.currentCount = 0;
+    this.loading = false;
+    this.more = true;
+    this.hasCollection = false;
+  }
 
   attached() {
     $('.accountmenu').removeClass('active');
+  }
+
+  activate(params, routeData) {
+    if ( routeData.campaign ) {
+      this.campaign = routeData.campaign;
+    }
+    else {
+      this.campaignServices.getCampaignByName(params.cname)
+        .then( (result) => {
+          this.campaign = new Campaign(result);
+      });
+    }
+    if ( routeData.collection ) {
+      this.collection = routeData.collection;
+      this.hasCollection = true;
+    }
   }
 }
