745506e7ea0f17cb28cd2edf78d20905e3020689
Spyros Bekiaris <spirosbekos@gmail.com>
2019-06-07 16:24:02 +0300

    Change metadata representation (in progress)

---
+++ src/widgets/metadata/metadata.js
@@ -0,0 +1,48 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+
+import { inject } from 'aurelia-framework';
+import { Router, activationStrategy } from 'aurelia-router';
+import { Record } from 'Record.js';
+import { UserServices } from 'UserServices';
+import { RecordServices } from 'RecordServices.js';
+import { toggleMore } from 'utils/Plugin.js';
+
+@inject(Router, UserServices, RecordServices)
+export class Metadata {
+
+  constructor(router, userServices, recordServices) {
+    this.router = router;
+    this.userServices = userServices;
+    this.recordServices = recordServices;
+
+    this.record = null;
+
+    // this.loadRec = false;
+    // this.more = true;
+  }
+
+  activate(params) {
+    this.record = params.record;
+
+    //this.content = params.record.data.content.JSONLD-EDM;
+    // console.log(this.record);
+  }
+
+  toggleLoadMore(container) {
+    toggleMore(container);
+  }
+}
