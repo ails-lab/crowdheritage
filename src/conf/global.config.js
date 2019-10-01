f2ad3adeeb913369b2f485e6820a098b3d74ab1c
Spyros Bekiaris <spirosbekos@gmail.com>
2019-10-01 17:09:30 +0300

    Hide the list of annotated items inside the user-page

--- src/conf/global.config.js
+++ src/conf/global.config.js
@@ -30,7 +30,7 @@ var settings = {
 
 // Override settings for development/testing etc
 if (window.location.hostname === 'localhost') {
-	settings.baseUrl = 'https://api.withculture.eu';  // Original WITH-backend
+	// settings.baseUrl = 'https://api.withculture.eu';  // Original WITH-backend
 	// settings.baseUrl = 'https://api.withcrowd.eu';    // Backend with test-DB
 	// settings.baseUrl = 'http://localhost:9060';       // Local backend for testing
 	settings.project = 'CrowdHeritage';
