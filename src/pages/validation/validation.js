23269dda1f0d203c141a9d6c1cc3f839046ad1df
Spyros Bekiaris <spirosbekos@gmail.com>
2019-12-13 17:29:14 +0200

    [In Progress] Build the campaign validation page

--- src/pages/validation/validation.js
+++ src/pages/validation/validation.js
@@ -22,7 +22,7 @@ import { Record } from 'Record.js';
 import { RecordServices } from 'RecordServices.js';
 import { UserServices } from 'UserServices';
 import { I18N } from 'aurelia-i18n';
-//import settings from 'global.config.js';
+import settings from 'global.config.js';
 
 let instance = null;
 
@@ -30,29 +30,60 @@ let instance = null;
 export class Validation {
 
   constructor(campaignServices, recordServices, userServices, router, i18n) {
-	if (instance) {
-		return instance;
-	}
-	this.campaignServices = campaignServices;
-  this.recordServices = recordServices;
-	this.userServices = userServices;
-	this.router = router;
-	this.i18n = i18n;
+  	if (instance) {
+  		return instance;
+  	}
+    this.colorSet = [
+			["Black",       "background-color: #111111", "color: #111111; filter: brightness(500%);"],
+			["Grey",        "background-color: #AAAAAA","color: #AAAAAA; filter: brightness(60%);"],
+			["Brown",       "background-color: brown", "color:brown; filter: brightness(60%);"],
+			["Red",         "background-color: #FF4136","color: #FF4136; filter: brightness(60%);"],
+			["Orange",      "background-color: #FF851B", "color: #FF851B; filter: brightness(60%);"],
+			["Beige",       "background-color: beige", "color: beige; filter: brightness(60%);"],
+			["Yellow",      "background-color: #FFDC00", "color: #FFDC00; filter: brightness(60%);"],
+			["Green",       "background-color: #2ECC40", "color: #2ECC40; filter: brightness(60%);"],
+			["Blue",        "background-color: #0074D9", "color: #0074D9; filter: brightness(60%);"],
+			["Purple",      "background-color: #B10DC9", "color: #B10DC9; filter: brightness(60%);"],
+			["Pink",        "background-color: pink", "color: pink; filter: brightness(60%);"],
+			["White",       "background-color: #FFFFFF", "color: #FFFFFF; filter: brightness(60%);"],
+			["Copper",      "background-image: url(/img/color/copper.jpg)", "color: #b87333; filter: brightness(50%);"],
+			["Silver",      "background-image: url(/img/color/silver.jpg)", "color:  #DDDDDD; filter: brightness(30%);"],
+			["Bronze",      "background-image: url(/img/color/bronze.jpg)", "color: #cd7f32; filter: brightness(50%);" ],
+			["Gold",        "background-image: url(/img/color/gold.jpg)", "color: #FFD700; filter: brightness(50%);"],
+			["Multicolor",  "background-image: linear-gradient(to right, red,orange,yellow,green,blue,indigo,violet)", " color: white; text-shadow: 1px 1px 2px #424242;"],
+			["Transparent", "", "color: white; text-shadow: 1px 1px 2px #424242;"]
+		];
+  	this.campaignServices = campaignServices;
+    this.recordServices = recordServices;
+  	this.userServices = userServices;
+  	this.router = router;
+  	this.i18n = i18n;
 
-	this.loc;
-  this.campaignItem = null;
-  this.records = [];
-  this.label = "";
-  this.generators = [];
-  this.loadCamp = false;
-  this.loadRec = false;
-	if (!instance) {
-		instance = this;
-	}
+  	this.loc;
+    this.project = settings.project;
+
+    this.campaignItem = null;
+    this.records = [];
+    this.label = "";
+    this.generators = [];
+    this.loadCamp = false;
+    this.loadRec = false;
+    this.loading = false;
+  	if (!instance) {
+  		instance = this;
+  	}
   }
 
   get isAuthenticated() { return this.userServices.isAuthenticated(); }
   get user()            { return this.userServices.current; }
+
+  hasMotivation(name) {
+    return !!this.campaign.motivation.includes(name);
+  }
+
+  getColorLabel(label) {
+    return this.i18n.tr('item:color:'+label);
+  }
 
   attached() {
     $('.accountmenu').removeClass('active');
@@ -84,13 +115,23 @@ export class Validation {
 
     route.navModel.setTitle('Validation | ' + this.campaign.title);
 
-    this.label = "red";
-    this.generators = ["Image Analysis", "CrowdHeritage colours-catwalk"];
+
+	}
+
+  selectLabel(label) {
+    this.label = label.toLowerCase();
+    this.generators.splice(0, this.generators.length);
+    this.generators.push(this.project + " " + this.campaign.username);
+    if (this.campaign.username == "colours-catwalk") {
+      this.generators.push("Image Analysis");
+    }
+    console.log(this.label, this.generators);
     this.recordServices.getAnnotatedRecordsByLabel(this.label, this.generators)
       .then( response => {
         this.records = response;
         console.log(this.records);
+        this.loading = true;
       });
-	}
+  }
 
 }
