1e1037a0ef91f73559518a09b0e069ca9b7df4cd
Spyros Bekiaris <spirosbekos@gmail.com>
2020-03-01 13:54:36 +0200

    Update the campaign-constructor to handle null fields

--- src/modules/Campaign.js
+++ src/modules/Campaign.js
@@ -18,16 +18,9 @@ import settings from 'global.config.js';
 
 export class Campaign {
 
-  constructor(data) {
+  constructor(data, loc) {
     this.dbId = data.dbId;
     this.username = data.username;
-    this.title = data.title;
-    this.description = data.description;
-    this.banner = data.banner;
-    if (this.banner) {
-      if (!this.banner.startsWith('http')) {
-        this.banner=`${settings.baseUrl}${this.banner}`;}
-      }
     this.logo = data.logo;
     this.project = data.project;
     this.creators = data.creators;
@@ -40,17 +33,25 @@ export class Campaign {
     this.target = data.annotationTarget;
     this.created = data.annotationCurrent.created;
     this.approved = data.annotationCurrent.approved;
-    this.rejected = data.annotationCurrent.rejected;    
+    this.rejected = data.annotationCurrent.rejected;
     this.contributorsCount = Object.keys(data.contributorsPoints).length;
     this.totalCurrent = this.created + this.approved + this.rejected;
     this.percentage = Math.min(100, Math.floor(100 * this.totalCurrent / this.target));
-    this.userPoints = data.contributorsPoints;    
+    this.userPoints = data.contributorsPoints;
     this.targetCollections = data.targetCollections;
     this.badges = data.badges;
-		this.prizes = data.prizes;
-		this.instructions = data.instructions;
     this.vocabularies = data.vocabularies;
 
+    // Set the campaign banner
+    this.banner = data.banner;
+    if (this.banner) {
+      if (!this.banner.startsWith('http'))
+        this.banner=`${settings.baseUrl}${this.banner}`;
+    }
+    else {
+      this.banner='../../img/assets/img/content/bg-search-space.png'
+    }
+    // Set the campaign status, based on startDate & endDate
     var today = new Date();
     var start = new Date(this.startDate);
     var end = new Date(this.endDate);
@@ -64,6 +65,32 @@ export class Campaign {
     else if ( (today<start) && (today<end) ) {
       this.status = 'upcoming';
     }
-  }
+    // Based on the selected language, set the campaign {title, description, instructions, prizes}
+    if (data.title)
+      this.title = ( data.title[loc] ? data.title[loc] : data.title['en'] );
+    else
+      this.title = '';
+    if (data.description)
+      this.description = ( data.description[loc] ? data.description[loc] : data.description['en'] );
+    else
+      this.description = '';
+    if (data.instructions)
+      this.instructions = ( data.instructions[loc] ? data.instructions[loc] : data.instructions['en'] );
+    else
+      this.instructions = '';
+    this.prizes = {};
+    if (data.prizes) {
+      this.prizes.gold = ( data.prizes.gold[loc] ? data.prizes.gold[loc] : data.prizes.gold['en'] );
+      this.prizes.silver = ( data.prizes.silver[loc] ? data.prizes.silver[loc] : data.prizes.silver['en'] );
+      this.prizes.bronze = ( data.prizes.bronze[loc] ? data.prizes.bronze[loc] : data.prizes.bronze['en'] );
+      this.prizes.rookie = ( data.prizes.rookie[loc] ? data.prizes.rookie[loc] : data.prizes.rookie['en'] );
+    }
+    else {
+      this.prizes.gold = '';
+      this.prizes.silver = '';
+      this.prizes.bronze = '';
+      this.prizes.rookie = '';
+    }
 
+  }
 }
