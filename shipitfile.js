ae76f62b0ba6d2b368b12db669ceb05815ac8696
Maria Ralli <mariai.ralli@gmail.com>
2020-05-15 11:36:29 +0300

    Do not use cwd as workspace for shipit

--- shipitfile.js
+++ shipitfile.js
@@ -3,55 +3,66 @@
 var chalk = require('chalk');
 
 module.exports = function(shipit) {
-	require('shipit-deploy')(shipit);
-	// require('shipit-npm')(shipit);
+    require('shipit-deploy')(shipit);
+    // require('shipit-npm')(shipit);
 
-	shipit.initConfig({
-		default: {
-			workspace: '/tmp/crowdheritage',
-			dirToCopy: '/tmp/crowdheritage/dist',
-			repositoryUrl: 'git@github.com:ails-lab/crowdheritage.git',
-			ignores: ['.git', 'node_modules'],
-			rsync: ['--del'],
-			keepReleases: 4,
-			key: '~/.ssh/id_rsa',
-			shallowClone: true,
-			npm: {
-				remote: false,
-				triggerEvent: 'fetched'
-			}
-		},
-		staging: {
-			servers: 'deploy@ipa.image.ntua.gr',
-			deployTo: '/data/withfrontend',
-			branch: 'master'
-		},
-		production: {
-			servers: 'deploy@playmobil.image.ntua.gr',
-			deployTo: '/data/withcrowd/frontend',
-			branch: 'master'
-		}
-	});
+    shipit.initConfig({
+        default: {
+            dirToCopy: 'dist',
+            repositoryUrl: 'git@github.com:ails-lab/crowdheritage.git',
+            ignores: ['.git', 'node_modules'],
+            rsync: ['--del'],
+            keepReleases: 4,
+            key: '~/.ssh/id_rsa',
+            // Setting shallowClone makes workspace obsolete.
+            // See https://github.com/shipitjs/shipit/issues/194
+            shallowClone: true,
+            npm: {
+                remote: false,
+                triggerEvent: 'fetched'
+            }
+        },
+        staging: {
+            servers: 'deploy@ipa.image.ntua.gr',
+            deployTo: '/data/withfrontend',
+            branch: 'master'
+        },
+        production: {
+            servers: 'deploy@playmobil.image.ntua.gr',
+            deployTo: '/data/withcrowd/frontend',
+            branch: 'master'
+        }
+    });
 
-	shipit.on('fetched', function() {
-		shipit.start('yarn:install');
-	});
+    shipit.on('fetched', function() {
+        shipit.start('copyConfig');
+    });
 
-	shipit.blTask('yarn:install', function() {
-		return shipit.local('yarn install', { cwd: shipit.config.workspace }).then(function() {
-			shipit.log(chalk.green('Node modules installed.'));
-			shipit.start('npm:build');
-		}).catch(function(e) {
-			shipit.log(chalk.red(e));
-		});
-	});
+    shipit.blTask('copyConfig', function() {
+        shipit.log('Copying local configuration file...');
+        return shipit.local('cp src/conf/local.config.js ' + shipit.workspace + '/src/conf').then(function() {
+            shipit.start('yarn:install');
+        }).catch(function(e) {
+            shipit.log(chalk.read(e));
+        });
+    });
 
-	shipit.blTask('npm:build', function() {
-		shipit.log('Building production files...');
-		return shipit.local('npm run build', { cwd: shipit.config.workspace }).then(function() {
-			shipit.local('mv ' + shipit.config.workspace + '/img ' + shipit.config.workspace + '/dist');
-		}).catch(function(e) {
-			shipit.log(chalk.red(e));
-		});
-	});
+    shipit.blTask('yarn:install', function() {
+        shipit.log('Installing Node modules...');
+        return shipit.local('yarn install', { cwd: shipit.workspace }).then(function() {
+            shipit.log(chalk.green('Node modules installed.'));
+            shipit.start('npm:build');
+        }).catch(function(e) {
+            shipit.log(chalk.red(e));
+        });
+    });
+
+    shipit.blTask('npm:build', function() {
+        shipit.log('Building production files...');
+        return shipit.local('npm run build', { cwd: shipit.workspace }).then(function() {
+            shipit.local('mv ' + shipit.workspace + '/img ' + shipit.workspace + '/dist');
+        }).catch(function(e) {
+            shipit.log(chalk.red(e));
+        });
+    });
 };
