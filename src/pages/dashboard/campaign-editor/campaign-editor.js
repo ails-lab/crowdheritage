420b63396786e8b45d437ed59c64ad618d927e4d
aposkarag <aposkarag@islab.ece.ntua.gr>
2021-10-26 15:27:10 +0300

    distinct pages for Campaign and Collection Editor

---
+++ src/pages/dashboard/campaign-editor/campaign-editor.js
@@ -0,0 +1,12 @@
+import { inject, LogManager, NewInstance } from 'aurelia-framework';
+import { ValidationController, ValidationRules } from 'aurelia-validation';
+
+let logger = LogManager.getLogger('CampaignEditor.js');
+
+let COUNT = 10;
+
+@inject()
+export class CampaignEditor {
+  
+
+}
---
+++ src/pages/dashboard/collection-editor/collection-editor.js
@@ -0,0 +1,12 @@
+import { inject, LogManager, NewInstance } from 'aurelia-framework';
+import { ValidationController, ValidationRules } from 'aurelia-validation';
+
+let logger = LogManager.getLogger('CollectionEditor.js');
+
+let COUNT = 10;
+
+@inject()
+export class CollectionEditor {
+  
+
+}
