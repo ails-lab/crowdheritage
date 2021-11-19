7310b662fb9a3cdf708ff4f4968ccb92763a8db4
Konstantinos T <konstantinos.thomas@gmail.com>
2021-11-19 21:26:54 +0200

    edit campaign page, no API calls

--- src/pages/dashboard/campaign-editor/campaign-cname-edit/campaign-cname-edit.js
+++ src/pages/dashboard/campaign-editor/campaign-cname-edit/campaign-cname-edit.js
@@ -2,25 +2,43 @@ import { inject } from 'aurelia-framework';
 import { Router } from 'aurelia-router';
 import { Collection } from 'Collection.js';
 import { CollectionServices } from 'CollectionServices.js';
+import { Campaign } from 'Campaign.js';
+import { CampaignServices } from 'CampaignServices.js';
 import { UserServices } from 'UserServices';
 import { I18N } from 'aurelia-i18n';
 //import settings from 'global.config.js';
 
 let instance = null;
 
-@inject(CollectionServices, UserServices, Router, I18N)
+@inject(CollectionServices, CampaignServices, UserServices, Router, I18N)
 export class CampaignCnameEdit {
 
-  constructor(collectionServices, userServices, router, i18n) {
+  constructor(collectionServices, campaignServices, userServices, router, i18n) {
 	if (instance) {
 		return instance;
 	}
-	this.collectionServices = collectionServices;
+	this.campaignServices = campaignServices;
 	this.userServices = userServices;
 	this.router = router;
-    this.i18n = i18n;
+  this.i18n = i18n;
 
-    this.loc;
+  this.loc;
+
+  // Initialization
+  this.title = '';
+  this.username = '';
+  this.description = '';
+  this.startDate = '';
+  this.endDate = '';
+  this.banner = '';
+  this.gender = '';
+  this.genders = [
+    { value: 'male',        name: this.i18n.tr('register:male') },
+    { value: 'female',      name: this.i18n.tr('register:female') },
+    { value: 'unspecified', name: this.i18n.tr('register:unspecified') }
+  ];
+  this.errors = {};
+
 	if (!instance) {
 		instance = this;
 	}
@@ -35,13 +53,22 @@ export class CampaignCnameEdit {
 
 	async activate(params, route) {
     this.loc = params.lang;
-	this.i18n.setLocale(params.lang);
+    this.i18n.setLocale(params.lang);
+    console.log(params);
 
-	this.cname = params.cname;
-	let collectionData = await this.collectionServices.getCollection(this.collectionId);
-	this.collection = new Collection(collectionData);
-    //route.navModel.setTitle(this.collection.title[0]+' | '+settings.project);
-    route.navModel.setTitle('Campaign | '+this.cname.title[0]);
+    this.cname = params.cname;
+    let campaignData = await this.campaignServices.getCampaignByName(this.cname);
+    this.campaign = new Campaign(campaignData);
+    //route.navModel.setTitle(this.campaign.title[0]+' | '+settings.project);
+    console.log(this.campaign)
+    route.navModel.setTitle('Campaign | '+this.campaign.title);
+
+    this.title = this.campaign.title;
+    this.username = this.campaign.username;
+    this.description = this.campaign.description;
+    this.startDate = this.campaign.startDate.replaceAll("/", "-");
+    this.endDate = this.campaign.endDate.replaceAll("/", "-");
+    this.banner = this.campaign.banner.replace("?file=true", "");
 	}
 
 }
