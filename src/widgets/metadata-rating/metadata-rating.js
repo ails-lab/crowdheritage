743c9a4820fc489d85cd69d110030fcc785a0076
aposkarag <aposkarag@islab.ece.ntua.gr>
2022-10-27 14:01:32 +0300

    implemented rating card outline with dummy text and rating range input

---
+++ src/widgets/metadata-rating/metadata-rating.js
@@ -0,0 +1,49 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+
+import { inject, LogManager } from 'aurelia-framework';
+import { DialogController } from 'aurelia-dialog';
+import { UserServices } from 'UserServices.js';
+import { Router } from 'aurelia-router';
+import { I18N } from 'aurelia-i18n';
+
+let logger = LogManager.getLogger('metadata-rating.js');
+
+@inject(DialogController, UserServices, Router, I18N)
+export class MetadataRating {
+
+	constructor(controller, userServices, router, i18n) {
+		this.controller = controller;
+		this.userServices = userServices;
+		this.router = router;
+		this.i18n = i18n;
+    this.rating = 0;
+    this.ratingText = '';
+	}
+  ratingChanged(){
+    this.ratingText = this.rating
+  }
+  ratingTextChanged(){
+    this.rating = this.ratingText
+    if (this.ratingText == ""){
+      this.rating = 0;
+    }
+  }
+  submit(){
+    // Submit rating
+  }
+
+}
