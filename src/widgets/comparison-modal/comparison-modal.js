b2e02151e6f5b06e6f2c000fd296ee1fdd0860b2
aposkarag <aposkarag@islab.ece.ntua.gr>
2023-03-29 15:01:52 +0300

    [CH-200] comparison workspace implemented

---
+++ src/widgets/comparison-modal/comparison-modal.js
@@ -0,0 +1,198 @@
+import { inject, LogManager } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
+import { I18N } from 'aurelia-i18n';
+
+let logger = LogManager.getLogger('comparison-modal.js');
+
+@inject(Router, I18N)
+export class ComparisonModal {
+
+  constructor(router, i18n) {
+    this.router = router;
+    this.i18n = i18n;
+    this.step = 1;
+    this.lens = null;
+    this.comparisonOriginalImage = null;
+    this.firstAlgorithmResult = null;
+    this.secondAlgorithmResult = null;
+    this.originalImageZoomed = null;
+    this.zoomInitiated = false;
+    this.firstSelected = false;
+    this.secondSelected = false;
+    this.submitted = false;
+  }
+
+  get locale() { return window.location.href.split('/')[3]; }
+
+  get stepLabel() {
+    if (this.step == 1) {
+      return '1st';
+    }
+    if (this.step == 2) {
+      return '2nd';
+    }
+    if (this.step == 3) {
+      return '3rd';
+    }
+    return this.step + 'th';
+  }
+  closeComparisonModal() {
+    if ((this.firstSelected || this.secondSelected || this.step!= 1) && !this.submitted &&
+      !confirm('If you close the workspace without completing all the comparisons your progress will be lost. Are you sure you want to proceed?')) return;
+    var modal = document.getElementById('comparisonModal');
+    var banner = document.getElementById("banner");
+    banner.style.display = "block";
+    modal.style.display = "none";
+    if (this.lens) {
+      this.stopZoom();
+    }
+    this.step = 1;
+    this.firstSelected = false;
+    this.secondSelected = false;
+    this.submitted = false;
+  }
+
+  nextStep() {
+    if (this.lens) {
+      this.stopZoom();
+    }
+    this.firstSelected = false;
+    this.secondSelected = false;
+    this.step++;
+  }
+
+  prevStep() {
+    if (this.lens) {
+      this.stopZoom();
+    }
+    this.firstSelected = false;
+    this.secondSelected = false;
+    this.step--;
+  }
+
+  initiateZoomLens() {
+    this.comparisonOriginalImage = document.getElementById('comparison-original-image');
+    /* Create lens: */
+    this.lens = document.createElement("DIV");
+    this.lens.setAttribute("class", "img-zoom-lens");
+    if (this.comparisonOriginalImage.width > this.comparisonOriginalImage.height) {
+      this.lens.style.height = "40px";
+      let ratio = this.comparisonOriginalImage.width / this.comparisonOriginalImage.height;
+      this.lens.style.width = (ratio * 40) + "px";
+    }
+    else {
+      this.lens.style.width = "40px";
+      let ratio = this.comparisonOriginalImage.height / this.comparisonOriginalImage.width;
+      this.lens.style.height = (ratio * 40) + "px";
+    }
+    /* Insert lens: */
+    this.comparisonOriginalImage.parentElement.insertBefore(this.lens, this.comparisonOriginalImage);
+  }
+
+  initiateZoom(e) {
+    if (!this.comparisonOriginalImage || !this.lens) {
+      this.initiateZoomLens();
+      this.zoomInitiated = true;
+    }
+    var pos, x, y;
+    /* Prevent any other actions that may occur when moving over the image */
+    e.preventDefault();
+    /* Get the cursor's x and y positions: */
+    pos = this.getCursorPos(e);
+    /* Calculate the position of the this.lens: */
+    x = pos.x + 1;
+    y = pos.y + 1;
+    /* Prevent the this.lens from being positioned outside the image: */
+    if (x > this.comparisonOriginalImage.width - this.lens.offsetWidth) { x = this.comparisonOriginalImage.width - this.lens.offsetWidth; }
+    if (x < 0) { x = 0; }
+    if (y > this.comparisonOriginalImage.height - this.lens.offsetHeight) { y = this.comparisonOriginalImage.height - this.lens.offsetHeight; }
+    if (y < 0) { y = 0; }
+    /* Set the position of the this.lens: */
+    this.lens.style.left = x + "px";
+    this.lens.style.top = y + "px";
+
+    let cx, cy, ratio = this.comparisonOriginalImage.width / this.comparisonOriginalImage.height;
+
+
+    this.firstAlgorithmResult = document.getElementById("first-algorithm-zoomed");
+    /* Calculate the ratio between this.firstAlgorithmResult DIV and lens: */
+    if (this.firstAlgorithmResult) {
+      this.firstAlgorithmResult.style.width = ratio * this.firstAlgorithmResult.offsetHeight + "px";
+      cx = this.firstAlgorithmResult.offsetWidth / this.lens.offsetWidth;
+      cy = this.firstAlgorithmResult.offsetHeight / this.lens.offsetHeight;
+      /* Set background properties for the this.firstAlgorithmResult DIV */
+      this.firstAlgorithmResult.style.backgroundImage = "url('" + this.comparisonOriginalImage.src + "')";
+      this.firstAlgorithmResult.style.backgroundSize = (this.comparisonOriginalImage.width * cx) + "px " + (this.comparisonOriginalImage.height * cy) + "px";
+      /* Display what the this.lens "sees": */
+      this.firstAlgorithmResult.style.backgroundPosition = "-" + (x * cx) + "px -" + (y * cy) + "px";
+    }
+
+    this.originalImageZoomed = document.getElementById("original-zoomed");
+    /* Calculate the ratio between this.originalImageZoomed DIV and this.lens: */
+    if (this.originalImageZoomed) {
+      this.originalImageZoomed.style.width = ratio * this.originalImageZoomed.offsetHeight + "px";
+      cx = this.originalImageZoomed.offsetWidth / this.lens.offsetWidth;
+      cy = this.originalImageZoomed.offsetHeight / this.lens.offsetHeight;
+      /* Set background properties for the this.originalImageZoomed DIV */
+      this.originalImageZoomed.style.backgroundImage = "url('" + this.comparisonOriginalImage.src + "')";
+      this.originalImageZoomed.style.backgroundSize = (this.comparisonOriginalImage.width * cx) + "px " + (this.comparisonOriginalImage.height * cy) + "px";
+      /* Display what the this.lens "sees": */
+      this.originalImageZoomed.style.backgroundPosition = "-" + (x * cx) + "px -" + (y * cy) + "px";
+    }
+
+    this.secondAlgorithmResult = document.getElementById("second-algorithm-zoomed");
+    /* Calculate the ratio between this.secondAlgorithmResult DIV and this.lens: */
+    if (this.secondAlgorithmResult) {
+
+      this.secondAlgorithmResult.style.width = ratio * this.secondAlgorithmResult.offsetHeight + "px";
+      cx = this.secondAlgorithmResult.offsetWidth / this.lens.offsetWidth;
+      cy = this.secondAlgorithmResult.offsetHeight / this.lens.offsetHeight;
+      /* Set background properties for the this.secondAlgorithmResult DIV */
+      this.secondAlgorithmResult.style.backgroundImage = "url('" + this.comparisonOriginalImage.src + "')";
+      this.secondAlgorithmResult.style.backgroundSize = (this.comparisonOriginalImage.width * cx) + "px " + (this.comparisonOriginalImage.height * cy) + "px";
+      /* Display what the this.lens "sees": */
+      this.secondAlgorithmResult.style.backgroundPosition = "-" + (x * cx) + "px -" + (y * cy) + "px";
+    }
+  }
+
+  getCursorPos(e) {
+    var a, x = 0, y = 0;
+    e = e || window.event;
+    /* Get the x and y positions of the image: */
+    a = this.comparisonOriginalImage.getBoundingClientRect();
+    /* Calculate the cursor's x and y coordinates, relative to the image: */
+    x = e.pageX - a.left;
+    y = e.pageY - a.top;
+    /* Consider any page scrolling: */
+    x = x - window.pageXOffset;
+    y = y - window.pageYOffset;
+    return { x: x, y: y };
+  }
+
+  stopZoom() {
+    let lens = document.getElementsByClassName("img-zoom-lens");
+    lens[0].remove()
+    this.lens = null;
+    this.firstAlgorithmResult = null;
+    this.secondAlgorithmResult = null;
+    this.originalImageZoomed = null;
+    this.zoomInitiated = false;
+  }
+
+  firstAlgoSelected() {
+    this.firstSelected = true;
+    this.secondSelected = false;
+  }
+
+  secondAlgoSelected() {
+    this.firstSelected = false;
+    this.secondSelected = true;
+  }
+
+  submitComparison() {
+    console.log("GVSGFUBVDFVS")
+    this.submitted = true;
+    this.closeComparisonModal();
+  }
+
+}
