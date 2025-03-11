fa50a1ec2544ddbdd3b59ca8bd98d2a6d297b4f1
Leonidas Mavrotas <leo.mav.8@gmail.com>
2025-03-11 14:57:07 +0200

    make the image of each item bigger

--- src/widgets/item-view/item-dataview.js
+++ src/widgets/item-view/item-dataview.js
@@ -44,7 +44,7 @@ export class ItemDataView {
     this.previous = params.previous;
     this.collectionTitle = params.collectionTitle;
     this.imageErrorCounter = 0;
-    this.noImageStyle = "";
+    this.noImageStyle = "width: 100%;";
     this.instructionsCollapsed =
       JSON.parse(localStorage.getItem("campaignInstructionsCollapsed")) ||
       false;
